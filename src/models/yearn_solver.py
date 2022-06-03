b84a76424c86dbdb2f8c24e107dbb2a5fe7dc8f4
Tonkers <tonkers.society@gmail.com>
2022-06-03 12:01:44 -0700

    fix: Use env variable rpc

--- src/models/yearn_solver.py
+++ src/models/yearn_solver.py
@@ -1,27 +1,27 @@
 from src.util.schema import (
     SettledBatchAuctionModel,
     InteractionData,
-    BigInt
 )
 from decimal import Decimal
 from web3 import Web3
 import json
-from eth_abi import encode_single, encode_abi
+import os
+from dotenv import load_dotenv
 
-url = 'https://rpc.xdaichain.com'
+load_dotenv()
+url = os.environ['NODE_URL']
 w3 = Web3(Web3.HTTPProvider(url))
 
 
 def solve(batch) -> SettledBatchAuctionModel:
+    print(f'Using Yearn simple solver')
     vault_raw = open('./contracts/artifacts/Vault.json')
 
     vault_abi = json.load(vault_raw)["abi"]
 
-    print(f'yearn solver')
     interaction_data = []
     prices = {}
     for order in batch.orders:
-        print(type(order.buy_token))
         buy_token = w3.eth.contract(address=Web3.toChecksumAddress(order.buy_token.value), abi=vault_abi)
         sell_token = w3.eth.contract(address=Web3.toChecksumAddress(order.sell_token.value), abi=vault_abi)
         sell_amount = int(order.sell_amount)
@@ -50,7 +50,6 @@ def solve(batch) -> SettledBatchAuctionModel:
             # order to sell yv for underlying
             pps = sell_token.functions.pricePerShare().call()
             dec = sell_token.functions.decimals().call()
-            print(pps)
             buy_amount = int(sell_amount * pps / (10 ** dec))
 
             withdraw_data = InteractionData(
@@ -63,15 +62,10 @@ def solve(batch) -> SettledBatchAuctionModel:
         else:
             # no yv involved
             continue
-        print(f'{type(buy_amount)} {type(sell_amount)} ')
         order.execute(buy_amount_value=buy_amount, sell_amount_value=sell_amount, xrate_tol=Decimal(5))
-        print(buy_amount)
 
         prices[order.sell_token] = sell_amount
         prices[order.buy_token] = buy_amount
-        print(batch)
-        print(prices)
-
     return SettledBatchAuctionModel(
         ref_token=batch.ref_token.value,
         orders={order.order_id: order.as_dict() for order in batch.orders if order.is_executed()},
